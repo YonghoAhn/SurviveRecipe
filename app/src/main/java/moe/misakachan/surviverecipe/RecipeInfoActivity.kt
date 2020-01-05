d0b1c722aad601ecd3a56e165bd410c8dcdcb991
chicken2039 <zoq2039@gmail.com>
2020-01-05 19:34:56 +0900

    recipe main

---
+++ app/src/main/java/moe/misakachan/surviverecipe/RecipeInfoActivity.kt
@@ -0,0 +1,12 @@
+package moe.misakachan.surviverecipe
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RecipeInfoActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_recipe_info)
+    }
+}
