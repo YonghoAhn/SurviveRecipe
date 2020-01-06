ecdc2b48e201e36a68c0f6576d5cd37dc7f1d74e
chicken2039 <zoq2039@gmail.com>
2020-01-06 23:48:52 +0900

    user profile and main page

---
+++ app/src/main/java/moe/misakachan/surviverecipe/MainPageActivity.kt
@@ -0,0 +1,12 @@
+package moe.misakachan.surviverecipe
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainPageActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_page)
+    }
+}
