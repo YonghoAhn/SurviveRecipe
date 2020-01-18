965b65b9f6c4f95ed9151810551963c675eceb81
chicken2039 <zoq2039@gmail.com>
2020-01-19 04:57:41 +0900

    [Add] Layout editing - custom_listview.xml, profile_listview.xml: applied text style to textView - fragment_intro.xml: removed - fragment_user_profile.xml: Changed ListView to RecyclerView - IntroFragment.kt: removed //add necessary of ingredient UI //add difficulty of recipe UI #add models

--- app/src/main/java/moe/misakachan/surviverecipe/util/MainRecyclerAdpater.kt
+++ app/src/main/java/moe/misakachan/surviverecipe/util/MainRecyclerAdpater.kt
@@ -6,15 +6,4 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import moe.misakachan.surviverecipe.model.RecipeList
 
-class MainRecyclerAdpater(val context: Context, val List, ArrayList<RecipeList>) :
-    RecyclerView.Adapter<RecipeList>() {
-    inner class Holder(itemView: View?) : RecyclerView.ViewHolder(itemView) {
-
-    }
-
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecipeList {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
-}
-
-}
+class MainRecyclerAdpater
