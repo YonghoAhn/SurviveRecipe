3903cff9186d01e8ab1a06e35b39618e2c799f63
chicken2039 <zoq2039@gmail.com>
2020-01-22 18:01:13 +0900

    [Add] Layout editing          add ProfileSettingActivity.kt          add activity_profile_setting.xml

---
+++ app/src/main/java/moe/misakachan/surviverecipe/activity/ProfileSettingActivity.kt
@@ -0,0 +1,12 @@
+package moe.misakachan.surviverecipe.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import moe.misakachan.surviverecipe.R
+
+class ProfileSettingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile_setting)
+    }
+}
